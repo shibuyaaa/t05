ca0a0b70d4d6fa89d83a20e8343c6d7af7dd6cf1
shibuyaaa <20jz0121@jec.ac.jp>
2022-10-26 12:04:10 +0900

    Update Test.php

--- app/Models/Test.php
+++ app/Models/Test.php
@@ -5,8 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-//shibuya
-
 class Test extends Model
 {
     use HasFactory;
